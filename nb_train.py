a31c152e4aa098c53ba09dabab1699624180764a
Khine <cherylsuu@gmail.com>
2022-07-06 11:10:36 -0500

    refactor nb_train.py to standardize operations on the model

--- nb_train.py
+++ nb_train.py
@@ -4,87 +4,159 @@ import re
 import nltk
 import random
 import pickle
-from sklearn.model_selection import train_test_split
 from nltk.corpus import stopwords
 from zipfile import ZipFile
 
-with ZipFile('imdb ratings.zip') as zf:
-    f = zf.open('movie.csv')
-    
-df = pd.read_csv(f)
-smalldf = df[:2000]
-smalldf = smalldf.drop_duplicates(ignore_index = True)
-smalldf.loc[smalldf['label'] == 0, 'label'] = 'neg'
-smalldf.loc[smalldf['label'] == 1, 'label'] = 'pos'
+DATA = 'movie.csv'
+FEATURE_0 = 'text'
+LABEL = 'label'
+LABEL_0 = 0
+LABEL_1 = 1
+NEGATIVE_SENTIMENT = 'neg'
+POSITIVE_SENTIMENT = 'pos'
+DATA_LENGTH = 2000
+STOPWORD_TYPE = 'english'
+TRAIN_DATA_LENGTH = 1500
+NUM_OF_FEATURES = 5
+FILE_NAME = 'nb_classifier'
 
-def word_processor(text):  
-    text = "".join([i.lower() for i in text.replace('<br', '') if i not in string.punctuation])
+class NaiveBayesClassifier:
+    def __init__(self, classifier): 
+        self.classifier = classifier 
+    
+    @classmethod
+    def from_pickled(cls, path):
+        '''Unpickles the file given by path, returns an instance of the class initialized with the classifier object.'''
+        
+        classifier = pickle.load(open(path,'rb'))
+        return cls(classifier)
+    
+    def test(self, test_data):
+        '''Evaluates the model on test data, and returns the accuracy.'''
+        
+        return nltk.classify.accuracy(classifier, test_data)
+    
+    def predict(self, str_input):
+        '''Takes in a string, splits it, and returns a prediction for the sentiment of that string.'''
+        
+        words = str_input.split(' ')
+        return classifier.classify(find_features(words, common_words))  # TODO: Will common_words defined in main() be visible to predict()?
+        
+    
+    def store(self, path):
+        '''Accepts a file path and stores the model at that file path using the pickle library.'''
+        
+        with open(path,'wb') as f:
+            pickle.dump(classifier, f)
+    
+
+def clean(text):
+    '''Converts the input text into lower case, removes <br> tags, punctuation, whitespace, and stopwords.
+    Returns a list of words that remain.
+    
+        Args:
+        text(str): input text'''
+    
+    text = ''.join([w.lower() for w in text.replace('<br', '') if w not in string.punctuation])
     text = re.sub('\s+',' ', text)
-    text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
+    text = [word for word in text.split() if word not in stopwords.words(STOPWORD_TYPE)]
     return text
 
-def rev_sent(df):  
-    '''pair processed review strings with their corresponding class labels in tuples, 
-    return the tuples in a randomly shuffled list'''
+def make_text_label_tuples(df): 
+    '''Make tuples that pair processed strings with their corresponding labels. Returns the tuples in a list.
+    
+        Args:
+        df: dataframe'''
     
     tup_list = []
     for i in range(len(df)):
-        new_string = word_processor(df.iloc[i]['text'])
-        sentiment = df.iloc[i]['label']
-        tup_list.append((new_string.split(' '), sentiment))
-    
-    random.shuffle(tup_list)
+        word_list = clean(df.iloc[i][FEATURE_0])
+        sentiment = df.iloc[i][LABEL]
+        tup_list.append((word_list, sentiment))  
     
     return tup_list 
 
-def commwords_extractor(tuplist, x):
-    '''get the x number of most common words out of the strings in tup_list, return the words in a list'''
+def extract_common_words(data, count):
+    '''Selects the specified number of most common words out of the strings in data, returns the words in a list.
     
-    review_words = [t[0] for t in tuplist]
-    words_flat = [word for rev in review_words for word in rev] 
-    all_words = nltk.FreqDist(w.lower() for w in words_flat) 
-    comm_words = list(all_words)[:x]
+        Args:
+        data(list): a list of words
+        count(int): the number of most common words to be extracted.'''
+       
+    word_frequency = nltk.FreqDist(w.lower() for w in data) 
+    common_words = list(word_frequency)[:count]
     
-    return comm_words
+    return common_words
 
-def review_features(review):
-    '''decompose a review into unique words, returns a dict mapping features to booleans if the feature is in a given review'''
+def find_features(word_list, feature_words):
+    '''Select unique words from a list of words, returns a dictionary mapping features to booleans.
+ 
+        Args:
+        word_list(list): a list of words split from a sentence
+        feature_words(list): a list of common words
+        
+        Returns:
+        features(dict): a dictionary with each word in feature_words as keys and booleans as values 
+        (T if the word appears in word_list, F if otherwise).'''
     
-    words = set(review) 
+    words = set(word_list)
     features = {}
-    for w in commwords:
+    for w in feature_words:  
         features[f'contains({w})'] = (w in words)
         
     return features
 
 def train_model(data):
-    '''Train the model, return the model'''
+    '''Trains the model, returns the model.
     
-    classifier = nltk.NaiveBayesClassifier.train(data)   
+        Args:
+        data(list): a list of tuples where the first element of each tuple is a dict of features, the second element is the sentiment.'''
+    
+    classifier = nltk.NaiveBayesClassifier.train(data)  
     
     return classifier
 
 
-#Pair review to corresponding sentiment
-review_to_sentiment = rev_sent(smalldf)
-
-#Extract 2000 most common words
-commwords = commwords_extractor(review_to_sentiment, 2000)
-
 def main():
     
-    featuresets = [(review_features(rev), label) for (rev, label) in review_to_sentiment]
-    train_set, test_set = featuresets[:1500], featuresets[1500:]
+    with ZipFile('imdb ratings.zip') as zf:
+        f = zf.open(DATA)
     
-    model_accuracy = nltk.classify.accuracy(train_model(train_set), test_set)
+    df = pd.read_csv(f)
+    smalldf = df[:DATA_LENGTH]
+    smalldf = smalldf.drop_duplicates(ignore_index = True)
+    smalldf.loc[smalldf[LABEL] == LABEL_0, LABEL] = NEGATIVE_SENTIMENT
+    smalldf.loc[smalldf[LABEL] == LABEL_1, LABEL] = POSITIVE_SENTIMENT
+    
+    
+    # Pair each string to its corresponding sentiment.
+    str_to_sentiment = make_text_label_tuples(smalldf)  
+    
+    random.shuffle(str_to_sentiment) # randomly shuffle the list of tuples to randomize the contents of train and test data.
+    
+    nested_words = [t[0] for t in str_to_sentiment] 
+    
+    words_flatlist = [word for wl in nested_words for word in wl]
+    
+    common_words = extract_common_words(words_flatlist, 2000)
+    
+    # TODO: should I only find features from the train data? Am I overfitting by also finding features from the test set?
+    
+    featuresets = [(find_features(word_list, common_words), label) for (word_list, label) in str_to_sentiment] 
+    
+    train_set, test_set = featuresets[:TRAIN_DATA_LENGTH], featuresets[TRAIN_DATA_LENGTH:]
+    
+    model_accuracy = nltk.classify.accuracy(train_model(train_set), test_set)  
+    
     print('NB model accuracy:', model_accuracy)
-    print(train_model(train_set).show_most_informative_features(5))
+    print(train_model(train_set).show_most_informative_features(NUM_OF_FEATURES))
     
-    #Save model
-    filename = 'nb_classifier'
-    outfile = open(filename,'wb')
-    pickle.dump(train_model(train_set), outfile)
-    outfile.close()
+    # Save model.
+    with open(FILE_NAME,'wb') as f:
+        pickle.dump(train_model(train_set), f)
+
+    
+    model = NaiveBayesClassifier(train_model(train_set))
     
 if __name__ == '__main__':
     main()
